3dfce789022943d368805a469d0d0a90a4b00a04
Osman Hussein <osman_990@hotmail.com>
2019-08-24 15:33:48 +0100

    first commit

---
+++ src/main/java/com/osman/sample/calendar/application92/App.java
@@ -0,0 +1,13 @@
+package com.osman.sample.calendar.application92;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
